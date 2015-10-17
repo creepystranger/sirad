2cc85dc3b14319f40fb728148c1c2b8425b7eec8
creepystranger <reachbiswarup@gmail.com>
2015-10-17 16:17:47 -0700

    Update bccal.R

--- R/bccal.R
+++ R/bccal.R
@@ -13,8 +13,8 @@ function (lat, days, rad_mea,extraT = NULL, Tmax, Tmin, tal)
     dtemp <- c(Tmax[-le] - (Tmin[-le] + Tmin[-1])/2, Tmax[le] - 
         (Tmin[le - 1] + Tmin[le])/2)
     Zdtemp <- zoo(Tmax - Tmin, order.by = days)
-    dtempM <- mean(as.numeric(aggregate(Zdtemp, by = format(time(Zdtemp), 
-        "%m"), FUN = mean, na.rm = TRUE)), na.rm = T)
+    dtempM <- mean(as.numeric(aggregate(Zdtemp, by = format(time(Zdtemp)), FUN = mean, na.rm = TRUE)), na.rm = T)
+    #second argument for format should be a logical value for trim(by default False)
     m <- nls(rad_mea ~ extraT * tal * (1 - exp((-b * dtemp^2)/dtempM)), 
         start = list(b = 0.05), trace = F,control = list(maxiter = 500))
     rval <- c(coef(m))
